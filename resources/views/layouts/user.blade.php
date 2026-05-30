dac6933eecc043aacba7b6683e95e73bcaeed3c7
GifarlyDals <gifarlydals@gmail.com>
2026-05-31 01:44:00 +0700

    V 1.0.0

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('admin/img/icon.svg') }}">
 
     <title>LaporBanjir - Dashboard</title>
 
